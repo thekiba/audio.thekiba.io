23eacfb9d8b2cc40d7b145cb320996d79d4e103e
TheKiba <agrekov@aforex.ru>
2015-09-08 21:43:05 +0200

    Update view

---
+++ assets/javascripts/application.js
@@ -0,0 +1,27 @@
+//= require vendors/jquery-2.1.4.min.js
+//= require vendors/bootstrap.min.js
+
+//= require vendors/jquery.flot.min.js
+//= require vendors/jquery.flot.resize.min.js
+//= require vendors/curvedLines.js
+//= require vendors/jquery.sparkline.min.js
+//= require vendors/jquery.easypiechart.min.js
+//= require vendors/chosen.jquery.min.js
+        
+//= require vendors/moment.min.js
+//= require vendors/fullcalendar.min.js
+//= require vendors/jquery.simpleWeather.min.js
+//= require vendors/jquery.autosize.min.js
+//= require vendors/jquery.nicescroll.min.js
+//= require vendors/waves.min.js
+//= require vendors/bootstrap-growl.min.js
+//= require vendors/bootstrap-select.min.js
+//= require vendors/sweet-alert.min.js
+        
+//= require theme/curved-line-chart.js
+//= require theme/line-chart.js
+//= require theme/charts.js
+
+//= require theme/functions.js
+
+//= require_tree ./application
---
+++ assets/javascripts/vendors/load.js
@@ -0,0 +1,5 @@
+//= require_tree ./moment
+//= require_tree ./waves
+//= require_tree ./sweet-alert
+//= require_tree ./jquery
+//= require_tree ./bootstrap
